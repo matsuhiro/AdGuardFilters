35c335fcca8f8cb6a034e3feabb7edc4c185e79d
HiroshiMatsunaga <hiroshi.82.matsunaga@gmail.com>
2025-05-03 15:27:57 +0900

    Add rakuten ad

--- make_all_filter.sh
+++ make_all_filter.sh
@@ -3,4 +3,4 @@ cd BlockLists
 rm -rf downloads
 ./download_all.sh
 cd ..
-python3 filter_domains.py annonymous.txt casino.txt drag.txt line.txt porn.txt suside.txt violence.txt bbs.txt comic.txt fishing.txt matching.txt social.txt wepon.txt yahoojp.txt
+python3 filter_domains.py annonymous.txt casino.txt drag.txt line.txt porn.txt suside.txt violence.txt bbs.txt comic.txt fishing.txt matching.txt social.txt wepon.txt yahoojp.txt rakuten.txt
